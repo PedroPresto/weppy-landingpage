afbc6c53a2716616beeb2ce0229f94a2263690c3
Pedro Presto <pedroprestodev@gmail.com>
2025-10-06 18:31:38 -0300

    add pixel fb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,6 @@ export default function Home() {
                 <FloatingWhatsAppButton />
                 <HeroSection onLoginClick={handleLoginClick} />
                 <HowItWorksSection />
-                <VslSection />
                 <CompaniesSection />
                 <FeaturesSection />
                 <TestimonialsSection />
